a6ee6b50246bbb20e2e1299c4a536930c50390ff
F74076213_WU <oscar9335@gmail.com>
2020-05-31 22:22:07 +0800

    i cannot place the picture

--- GameMap2.java
+++ GameMap2.java
@@ -1,15 +1,31 @@
-import java.awt.Color;
-import java.awt.Toolkit;
+import java.awt.*;
+import javax.swing.*;
 
-import javax.swing.JButton;
-import javax.swing.JPanel;
 public class GameMap2 extends JPanel{
 	public GameMap2() {
 		setSize(Toolkit.getDefaultToolkit().getScreenSize());
-		setBackground(Color.green);
-		JButton b=new JButton();
-		b.setSize(100,100);
-		add(b);
-		setVisible(true);
+		this.setLayout(null);
+		//setBackground(Color.green);
+		//JButton b=new JButton();
+		//b.setSize(100,100);
+		//add(b);
+		//setVisible(true);	
+		newBackground();
+	
+		}
+	private void newBackground() {
+		try {
+			JLabel jlb = new JLabel();
+			int width = 1300,height = 700;
+			ImageIcon image = new ImageIcon("Map2.png");		
+			image.setImage(image.getImage().getScaledInstance(width, height,Image.SCALE_DEFAULT ));	
+			jlb.setIcon(image);
+			jlb.setSize(width, height);
+			System.out.println("DeBug");
+			this.add(jlb);
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("fuck");
+		}
 	}
 }
