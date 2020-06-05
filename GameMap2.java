7a0506b2cb94df0ae0e9dd2eaa7923f17f4528ea
F74076213_WU <oscar9335@gmail.com>
2020-06-05 10:55:30 +0800

    嘗試用button

--- GameMap2.java
+++ GameMap2.java
@@ -2,21 +2,25 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class GameMap2 extends JPanel implements ActionListener, KeyListener{
+public class GameMap2 extends JPanel implements ActionListener{
 	public GameMap2() {
-		this.addKeyListener(this);
+		
+
 		setSize(Toolkit.getDefaultToolkit().getScreenSize());
-		this.setLayout(null);
+	
 		setFocusable(true);
+		
+		button_location();
  		store_buttom();
 		bag_buttom();
 		newBackground();
+		this.setLayout(null);
+	
+		setVisible(true);
+		
 		
 		
 		}
-	
-	private int bag_x = 1300;
-	private int bag_y = 700; 
 	
 	private void newBackground() {
 		JLabel jlb = new JLabel();
@@ -28,6 +32,78 @@ public class GameMap2 extends JPanel implements ActionListener, KeyListener{
 			
 		this.add(jlb);
 	}
+	
+	private int person_x = 750;
+	private int person_y = 280;
+	
+	
+	
+	private void button_location() {
+		ImageIcon csie = new ImageIcon("ball.png");
+		csie.setImage(csie.getImage().getScaledInstance(30, 30,Image.SCALE_DEFAULT ));
+		JButton center = new JButton(csie);
+		center.setContentAreaFilled(false);
+		JButton water = new JButton(csie);
+		water.setContentAreaFilled(false);
+		JButton grass_1 = new JButton(csie);
+		grass_1.setContentAreaFilled(false);
+		JButton grass_2 = new JButton(csie);
+		grass_2.setContentAreaFilled(false);
+		
+		ImageIcon pp = new ImageIcon("培英.jpg");
+		JButton pein = new JButton(pp);
+		
+		ImageIcon da = new ImageIcon("資結.jpg");
+		JButton data_s = new JButton(da);
+		
+		ImageIcon com = new ImageIcon("英超1.jpg");      //有2張圖到時候選
+		JButton computer = new JButton(com);
+		
+		
+		center.setSize(40, 40);
+		center.setLocation(750, 280);
+	
+		water.setSize(40, 40);
+		water.setLocation(100, 280);
+	
+		grass_1.setSize(40, 40);
+		grass_1.setLocation(100, 625);
+
+		grass_2.setSize(40, 40);
+		grass_2.setLocation(750, 625);
+	
+		pein.setSize(120, 162);
+		pein.setLocation(725, 0);
+		
+		data_s.setSize(100, 148);
+		data_s.setLocation(1310, 40);
+		
+		computer.setSize(120,154);
+		computer.setLocation(1250, 490);
+		
+		ImageIcon cc = new ImageIcon("正面1.png");  
+		JButton character = new JButton(cc);
+		character.setContentAreaFilled(false);
+		character.setSize(60,70);
+		character.setLocation(750-10, 280-40);      //再對照的時候就 x+10 y+40
+		character.setActionCommand("character");
+		character.addActionListener(this);
+		
+		//setVisible(true);
+		
+		this.add(character);
+		this.add(center);
+		this.add(water);
+		this.add(grass_1);
+		this.add(grass_2);
+		this.add(pein);
+		this.add(data_s);
+		this.add(computer);
+		
+		
+	}
+	
+	
 	private void store_buttom() {
 		ImageIcon store_image = new ImageIcon("Store_icon.png");		
 		store_image.setImage(store_image.getImage().getScaledInstance(100, 60,Image.SCALE_DEFAULT ));	
@@ -42,18 +118,19 @@ public class GameMap2 extends JPanel implements ActionListener, KeyListener{
 		this.add(store);
 	}
 	
-	ImageIcon bag_buttom = new ImageIcon("Bag_icon.png");		
-	//bag_buttom.setImage(bag_buttom.getImage().getScaledInstance(100, 60,Image.SCALE_DEFAULT ));	
-	JButton bag = new JButton(bag_buttom);
+	
 	
 	private void bag_buttom() {
-		
+		ImageIcon bag_buttom = new ImageIcon("Bag_icon.png");		
+		bag_buttom.setImage(bag_buttom.getImage().getScaledInstance(100, 60,Image.SCALE_DEFAULT ));	
+		JButton bag = new JButton(bag_buttom);
 		bag.setActionCommand("bag");
 		bag.setIcon(bag_buttom);
 		bag.setSize(100,60);
+		int bag_x = 1300;
+		int bag_y = 735; 
 		bag.setLocation(bag_x, bag_y);
-		//bag.addActionListener(this);
-		bag.addKeyListener(this);
+		bag.addActionListener(this);
 		bag.setContentAreaFilled(false);
 		setVisible(true);	
 		this.add(bag);
@@ -61,12 +138,14 @@ public class GameMap2 extends JPanel implements ActionListener, KeyListener{
 	public void actionPerformed(ActionEvent e) {
 		switch(e.getActionCommand()) {
 			case "store":	
-					System.out.println("open store");
+				System.out.println("open store");	
 				break;
 			case "bag":
-					
-					System.out.println("open bag");
+				System.out.println("open bag");
 				break;		
+			case "character":
+				System.out.println("move");
+				break;
 		}
 	}
 	/*private Rectangle boy = new Rectangle(500,250,80,90);
@@ -76,32 +155,6 @@ public class GameMap2 extends JPanel implements ActionListener, KeyListener{
 	}*/
 	
 	
-	@Override
-	public void keyPressed(KeyEvent e) {
-		System.out.println("key keyTyped code:" + e.getKeyCode());
-		System.out.println("eeeeeeeeeeeeeeeeeeeeee");
-		if(e.getKeyCode() == KeyEvent.VK_UP) {
-			bag.setLocation(bag_x , bag_y-5);
-		}
-		if(e.getKeyCode() == KeyEvent.VK_DOWN) {
-			bag.setLocation(bag_x , bag_y+5);;
-		}
-		if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
-			bag.setLocation(bag_x +5 , bag_y);
-		}
-		if(e.getKeyCode() == KeyEvent.VK_LEFT) {
-			bag.setLocation(bag_x-5 , bag_y);
-		}
-	
-		}
-	@Override
-	public void keyReleased(KeyEvent e) {
-		System.out.println("key keyTyped code:" + e.getKeyCode());
-	}
-	@Override
-	public void keyTyped(KeyEvent e) {
-		System.out.println("key keyTyped code:" + e.getKeyCode());
-	}
 	
 	
 	
