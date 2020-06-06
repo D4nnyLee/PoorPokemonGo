47be94743ae4f143c9082be515808f8204f213ee
F74076213_WU <oscar9335@gmail.com>
2020-06-06 17:02:04 +0800

    fix

--- Mainwindows.java
+++ Mainwindows.java
@@ -1,17 +1,35 @@
 import javax.swing.*;
+import java.awt.event.*;
 public class Mainwindows extends JFrame {
 	Mainwindows() {
 		setExtendedState(JFrame.MAXIMIZED_BOTH);
 		setVisible(true);
 		setLayout(null);
 		GameMap1 map1 = new GameMap1();	
-		GameMap2 map2 = new GameMap2();		
+		GameMap2 map2 = new GameMap2();
 		Store store = new Store();		
 		Battle battle = new Battle();		
 		add(map1);
 		add(map2);
 		add(battle);
 		add(store);
+		
+		map2.requestFocusInWindow();
+		map2.addFocusListener(new FocusListener() {
+
+			@Override
+			public void focusGained(FocusEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void focusLost(FocusEvent e) {
+				// TODO Auto-generated method stub
+				map2.requestFocusInWindow();
+			}
+			
+		});
 	}
 	
 }
