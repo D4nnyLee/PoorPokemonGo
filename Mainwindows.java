8843635ad0330adc5dae7c5b0c4838a81df5e7e8
zzzz20102xx <zz20102aa@gmail.com>
2020-06-10 22:44:06 +0800

    fix something

--- Mainwindows.java
+++ Mainwindows.java
@@ -4,24 +4,24 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class Mainwindows extends JFrame {
-	
+
 	Mainwindows() {
-		Status player=new Status("player");
-		setExtendedState(JFrame.MAXIMIZED_BOTH);
+		Status player = new Status("player");
+		setSize(0, 0);
 		setVisible(true);
 		setLayout(null);
-		
-		GameMap2 map2 = new GameMap2(this,player);
-		
+
+		GameMap2 map2 = new GameMap2(this, player);
+
 		add(map2);
-		
+
 		map2.requestFocusInWindow();
 		map2.addFocusListener(new FocusListener() {
 
 			@Override
 			public void focusGained(FocusEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
 
 			@Override
@@ -29,7 +29,7 @@ public class Mainwindows extends JFrame {
 				// TODO Auto-generated method stub
 				map2.requestFocusInWindow();
 			}
-			
+
 		});
 
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
